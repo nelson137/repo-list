8cc1a7a11359262d74d93cc242d2af59d5d16070
Nelson Earle <nelson.earle137@gmail.com>
2022-07-19 14:11:35 -0500

    WIP: refactor repo card for drag & drop

---
+++ src/lib/ui/svgs/ExternalLink.svelte
@@ -0,0 +1,19 @@
+<!-- Source: https://tablericons.com/ -->
+<!-- Search: external-link -->
+<svg
+    xmlns="http://www.w3.org/2000/svg"
+    class="icon icon-external-link"
+    width="22"
+    height="22"
+    viewBox="0 0 24 24"
+    stroke-width="1.5"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke="#2c3e50"
+    fill="none"
+>
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M11 7h-5a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-5" />
+    <line x1="10" y1="14" x2="20" y2="4" />
+    <polyline points="15 4 20 4 20 9" />
+</svg>
