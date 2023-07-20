c39487570dca83c726efa31b6328dd6aee9da729
Nelson Earle <nelson.earle137@gmail.com>
2023-07-19 21:28:54 -0500

    augment env vars type

--- src/app.d.ts
+++ src/app.d.ts
@@ -5,6 +5,12 @@ export { };
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
+    interface ImportMetaEnv {
+        APP_GITHUB_ACCESS_TOKEN: string;
+        APP_CLIENT_ID: string;
+        APP_CLIENT_SECRET: string;
+    }
+
     declare namespace App {
         interface Locals {
             token: string;
