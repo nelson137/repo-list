4225ff1f18eba7f2df63a9b572a3d5559214a74b
Nelson Earle <nelson.earle137@gmail.com>
2023-07-19 22:12:19 -0500

    remove /login/callback data loader

--- src/routes/login/callback/+page.ts
+++
@@ -1,5 +0,0 @@
-import { handle_endpoint_err } from '$lib/error';
-import type { PageLoad } from './$types';
-
-export const load: PageLoad = ({ data }) =>
-    data?.error ? handle_endpoint_err(data?.error) : { logged_in: false };
