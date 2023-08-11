0a67c380593634f6a6ff19b185d31a57d3a60873
Nelson Earle <nelson.earle137@gmail.com>
2023-08-11 14:54:18 -0500

    remove class-transformer

--- src/lib/models/_model.d.ts
+++
@@ -1,7 +0,0 @@
-import { type ClassConstructor } from 'class-transformer';
-
-export declare function from_json<Class, Data>(
-    klass: ClassConstructor<Class>,
-    data: Data[]
-): Class[];
-export declare function from_json<Class, Data>(klass: ClassConstructor<Class>, data: Data): Class;
--- src/lib/models/_model.js
+++
@@ -1,7 +0,0 @@
-import { plainToInstance } from 'class-transformer';
-
-export function from_json(klass, data) {
-    return plainToInstance(klass, data, {
-        excludeExtraneousValues: true,
-    });
-}
