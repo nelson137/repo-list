2e0a81e9832555148449b0d258879382bf15ef25
Nelson Earle <nelson.earle137@gmail.com>
2023-09-05 12:55:43 -0500

    debug buttons: respect edit mode

--- src/lib/ui/debug/DebugDataButtons.svelte
+++ src/lib/ui/debug/DebugDataButtons.svelte
@@ -1,48 +1,38 @@
 <script lang="ts">
-    import { RepoListStorage } from '$lib/models/repo-list';
-    import {
-        RepositoryListsData,
-        load_local_storage,
-        write_to_local_storage,
-    } from '$lib/stores/repo-lists';
-    import { repos } from '$lib/stores/repos';
+    import { RepoList } from '$lib/models/repo-list';
+    import { edit_mode } from '$lib/stores/edit';
+    import { repo_lists } from '$lib/stores/repo-lists';
+    import { get } from 'svelte/store';
 
-    function load_store() {
-        load_local_storage(repos.get_all());
+    function delete_all_lists() {
+        get(repo_lists.lists).forEach(l => repo_lists.delete_list(l.id));
     }
 
     function on_click_clear() {
-        write_to_local_storage(new RepositoryListsData());
-        load_store();
+        delete_all_lists();
+        if (!get(edit_mode)) {
+            repo_lists.write_to_local_storage();
+        }
     }
 
     function on_click_defaults() {
-        const rld = new RepositoryListsData();
-        rld.load([
-            new RepoListStorage({
-                index: 0,
-                name: 'Pinned',
-                id: 'e4c45726-212e-44ce-bfaf-adf6d371ca4d',
-                repo_ids: [102030669, 532099005, 202956385],
-            }),
-            new RepoListStorage({
-                index: 1,
-                name: 'Games',
-                id: '5289b461-a49b-481a-b64f-1206fb73217b',
-                repo_ids: [529015461, 532099005, 79787322, 486775307, 486736755],
-            }),
-            new RepoListStorage({
-                index: 2,
-                name: 'School',
-                id: 'a67dc1b2-ae36-4c5c-a071-60d1b0f90701',
-                repo_ids: [
+        delete_all_lists();
+        repo_lists.add_list(
+            RepoList.from(
+                'School',
+                [
                     258009459, 530779943, 399959884, 439224243, 481792595, 332090987, 414866981,
                     558219478, 486775307, 486736755,
-                ],
-            }),
-        ]);
-        write_to_local_storage(rld);
-        load_store();
+                ]
+            )
+        );
+        repo_lists.add_list(
+            RepoList.from('Games', [529015461, 532099005, 79787322, 486775307, 486736755])
+        );
+        repo_lists.add_list(RepoList.from('Pinned', [102030669, 532099005, 202956385]));
+        if (!get(edit_mode)) {
+            repo_lists.write_to_local_storage();
+        }
     }
 </script>
 
