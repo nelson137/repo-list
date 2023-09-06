f94b12a0d72a7a16c7f89efda9515ad77c8aa91b
Nelson Earle <nelson.earle137@gmail.com>
2023-09-05 22:49:50 -0500

    warn of unsaved changes on unload in edit mode

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,10 +7,11 @@
     import Modal from 'svelte-simple-modal';
     import type { PageData } from './$types';
     import { page } from '$app/stores';
-    import { repo_lists, lists, load_local_storage } from '$lib/stores/repo-lists';
+    import { repo_lists, lists, load_local_storage, in_edit_mode } from '$lib/stores/repo-lists';
     import RepoListCard from './RepoListCard.svelte';
     import { Repo } from '$lib/models/repo';
     import DebugDataButtons from '$lib/ui/debug/DebugDataButtons.svelte';
+    import { get } from 'svelte/store';
 
     export let data: PageData;
 
@@ -18,9 +19,25 @@
         load_local_storage(Repo.parse_array($page.data.repos));
     });
 
+    function on_before_unload(event: BeforeUnloadEvent) {
+        if (!get(in_edit_mode)) return;
+        event.preventDefault();
+        /**
+         * Required for browser compatibility.
+         * The spec only requires `preventDefault` to trigger the confirmation
+         * dialogue, but most browsers don't yet support this. Most browsers
+         * still require (the deprecated) `returnValue` be set to a string.
+         * See MDN for browser support of `preventDefault`-only activation:
+         * https://developer.mozilla.org/en-US/docs/Web/API/Window/beforeunload_event#browser_compatibility
+         */
+        return (event.returnValue = '');
+    }
+
     const create_list = (event: CustomEvent<CreateListOkData>) =>
         repo_lists.add_list(RepoList.from(event.detail.name));
 </script>
+
+<svelte:window on:beforeunload={on_before_unload} />
 
 {#if data?.logged_in}
     {#if import.meta.env.DEV}
