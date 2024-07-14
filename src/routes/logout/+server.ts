e4be243ef61de7c3770da3af37ccfe2b2087cac3
Nelson Earle <nelson.earle137@gmail.com>
2024-07-13 19:52:47 -0500

    fix(deprecation): delete cookies by path

--- src/routes/logout/+server.ts
+++ src/routes/logout/+server.ts
@@ -2,6 +2,6 @@ import { redirect } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = ({ cookies }) => {
-    cookies.delete('token', { maxAge: 0 });
+    cookies.delete('token', { path: '/', maxAge: 0 });
     throw redirect(302, '/');
 };
